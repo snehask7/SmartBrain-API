47f042f4b8e60d653150b47965e7a2c245a14ee5
snehask7 <snehaskannan@gmail.com>
2020-04-05 02:32:45 +0530

    changed db conn

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-globular-12791',
-		user:'sneha',
-		password: '2001',
-		database: 'smart-brain'
+		connectionString: 'postgresql-globular-12791',
+		ssl: true;
 	}
 });
 
