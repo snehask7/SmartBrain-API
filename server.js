c31b351770f1627a87f6eeded53b758cd126824e
snehask7 <snehaskannan@gmail.com>
2020-04-05 02:53:24 +0530

    changed db conn

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		connectionString: 'postgresql-globular-12791',
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
